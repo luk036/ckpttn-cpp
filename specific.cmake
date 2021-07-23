886a3b03bd36041ee992e49e1744ec1987c14741
Wai-Shing Luk <luk036@gmail.com>
2021-07-24 00:14:01 +0800

    use latest xnetwork that removes unnecessary boost dependence (2)

--- specific.cmake
+++ specific.cmake
@@ -36,7 +36,7 @@ endif()
 
 CPMAddPackage(
   NAME XNetwork
-  GIT_TAG 1.0.10
+  GIT_TAG 1.0.11
   GITHUB_REPOSITORY luk036/xnetwork-cpp
   OPTIONS "INSTALL_ONLY ON" # create an installable target
 )
