1f905262e7168f6757a9bd28bdb3003a64894ce9
Wai-Shing Luk <luk036@gmail.com>
2021-11-13 04:39:57 +0000

    reformat using --target fix-format

--- specific.cmake
+++ specific.cmake
@@ -4,30 +4,32 @@ find_package(Threads REQUIRED)
 find_package(Boost REQUIRED COMPONENTS container)
 if(Boost_FOUND)
   message(STATUS "Found boost: ${Boost_LIBRARIES}")
-  # add_library(Boost::boost INTERFACE IMPORTED GLOBAL) target_include_directories(Boost::boost
-  # SYSTEM INTERFACE ${Boost_INCLUDE_DIRS})
+  # add_library(Boost::boost INTERFACE IMPORTED GLOBAL)
+  # target_include_directories(Boost::boost SYSTEM INTERFACE
+  # ${Boost_INCLUDE_DIRS})
 endif()
 
-CPMAddPackage(
-  NAME fmt
-  GIT_TAG 6.1.2
-  GITHUB_REPOSITORY fmtlib/fmt
-  OPTIONS "FMT_INSTALL YES" # create an installable target
+cpmaddpackage(
+  NAME
+  fmt
+  GIT_TAG
+  6.1.2
+  GITHUB_REPOSITORY
+  fmtlib/fmt
+  OPTIONS
+  "FMT_INSTALL YES" # create an installable target
 )
 
-CPMAddPackage("gh:microsoft/GSL@3.1.0")
-CPMAddPackage("gh:ericniebler/range-v3#0.10.0")
+cpmaddpackage("gh:microsoft/GSL@3.1.0")
+cpmaddpackage("gh:ericniebler/range-v3#0.10.0")
 
-CPMAddPackage(
-  NAME cppcoro
-  GIT_TAG 1.0
-  GITHUB_REPOSITORY luk036/cppcoro
-)
+cpmaddpackage(NAME cppcoro GIT_TAG 1.0 GITHUB_REPOSITORY luk036/cppcoro)
 # print_target_properties(cppcoro)
 if(cppcoro_ADDED)
   message(STATUS "Found cppcoro: ${cppcoro_SOURCE_DIR}")
   add_library(cppcoro::cppcoro INTERFACE IMPORTED GLOBAL)
-  target_include_directories(cppcoro::cppcoro SYSTEM INTERFACE ${cppcoro_SOURCE_DIR}/include)
+  target_include_directories(cppcoro::cppcoro SYSTEM
+                             INTERFACE ${cppcoro_SOURCE_DIR}/include)
 endif(cppcoro_ADDED)
 
 if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
@@ -41,11 +43,15 @@ else() # (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
   add_compile_options(-fcoroutines-ts -stdlib=libc++)
 endif()
 
-CPMAddPackage(
-  NAME XNetwork
-  GIT_TAG 1.0.16
-  GITHUB_REPOSITORY luk036/xnetwork-cpp
-  OPTIONS "INSTALL_ONLY ON" # create an installable target
+cpmaddpackage(
+  NAME
+  XNetwork
+  GIT_TAG
+  1.0.16
+  GITHUB_REPOSITORY
+  luk036/xnetwork-cpp
+  OPTIONS
+  "INSTALL_ONLY ON" # create an installable target
 )
 
 set(SPECIFIC_LIBS
@@ -56,5 +62,4 @@ set(SPECIFIC_LIBS
     Threads::Threads
     GSL
     fmt::fmt
-    range-v3
-)
+    range-v3)
