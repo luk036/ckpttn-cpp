6b241c239668f941d2c5770daf060464c1c2e278
Wai-Shing Luk <luk036@gmail.com>
2021-12-01 14:05:05 +0800

    select memory_resource

--- include/ckpttn/FMPmrConfig.hpp
+++ include/ckpttn/FMPmrConfig.hpp
@@ -1,15 +1,16 @@
 #pragma once
 
-// #if __has_include(<memeory_resource>)
+
+#if __has_include(<memory_resource>)
 #include <memory_resource>
 #include <vector>
 namespace FMPmr = std::pmr;
-// #else
-// #include <boost/container/pmr/memory_resource.hpp>
-// #include <boost/container/pmr/monotonic_buffer_resource.hpp>
-// #include <boost/container/pmr/vector.hpp>
-// namespace FMPmr = boost::container::pmr;
-// #endif
+#else
+#include <boost/container/pmr/memory_resource.hpp>
+#include <boost/container/pmr/monotonic_buffer_resource.hpp>
+#include <boost/container/pmr/vector.hpp>
+namespace FMPmr = boost::container::pmr;
+#endif
 
 const auto FM_MAX_NUM_PARTITIONS = 255U;
 const auto FM_MAX_DEGREE = 65536U;
